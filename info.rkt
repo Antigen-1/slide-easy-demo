ec6ff11d43be232afdb29e03b7f9cb139f3f280f
Antigen-1 <zhanghao@antigen.top>
2023-06-01 23:37:38 +0800

    fix deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "slide-easy-demo")
-(define deps '("base"))
+(define deps '("base" "slide-easy"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/slide-easy-demo.scrbl" ())))
 (define pkg-desc "Description Here")

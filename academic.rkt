dc839186269ffa8d727f825dfb306f54dc27900d
Antigen-1 <zhanghao@antigen.top>
2023-06-01 13:56:26 +0800

    fix the calling convention

--- academic.rkt
+++ academic.rkt
@@ -17,7 +17,7 @@
   (define (handler p proc)
     (if (pict? p) p (proc p)))
 
-  (define (->pict ls) (apply apply-generic ls))
+  (define (->pict ls) (apply-generic (car ls) ls))
 
   (define BLACK (make-object color% "Black"))
   
@@ -27,27 +27,34 @@
 
   (define background (blank 1000 700))
   
-  (define (封面->pict title member . rest)
+  (define (封面->pict title member)
     (define mb (handler member normalt))
     (define tt (handler title (lambda (str) (titlet str (make-object color% (current-theme-color))))))
     (lc-superimpose background (vl-append (current-gap-size) tt (hline (floor (/ (+ (pict-width tt) (pict-width mb)) 2)) (current-gap-size)) mb)))
 
-  (define (节->pict index title . rest)
+  (define (节->pict index title)
     (define id (handler index titlet))
     (define tt (handler title (lambda (str) (titlet str (make-object color% (current-theme-color))))))
     (cc-superimpose background (vc-append (current-gap-size) id (hline (floor (/ (+ (pict-width tt) (pict-width id)) 2)) (current-gap-size)) tt)))
 
-  (define/contract (图示->pict intr image source . rest)
-    (-> any/c pict? any/c any/c ... any)
+  (define/contract (图示->pict intr image source)
+    (-> any/c pict? any/c any)
     (cc-superimpose background (vc-append (current-gap-size)
                                           (handler intr (lambda (str) (titlet str (make-object color% (current-theme-color)))))
                                           image
                                           (handler source smallt))))
 
-  (define (致谢->pict member . rest)
+  (define (致谢->pict member)
     (封面->pict "THANKS FOR LISTENING" member))
 
+  (define (make-handler p)
+    (lambda (ls)
+      (apply p (cdr ls))))
+
   (install type (cons/c (and/c symbol? symbol-interned?) (listof (or/c string? pict?))) ->pict
-           (cons '封面 封面->pict) (cons '节 节->pict) (cons '图示 图示->pict) (cons '致谢 致谢->pict))
+           (cons '封面 (make-handler 封面->pict))
+           (cons '节 (make-handler 节->pict))
+           (cons '图示 (make-handler 图示->pict))
+           (cons '致谢 (make-handler 致谢->pict)))
   
   create)

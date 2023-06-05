d74b8a100368078135ac780496b674d9f8c1fe37
Antigen-1 <zhanghao@antigen.top>
2023-06-05 10:02:47 +0800

    using subtypes

--- academic.rkt
+++ academic.rkt
@@ -2,7 +2,7 @@
 (require (submod slide-easy generic) racket/draw racket/class)
 (provide (contract-out (current-theme-color (parameter/c (or/c string? (is-a?/c color%))))
                        (current-theme-font (parameter/c (or/c font-family/c (is-a?/c font%))))
-                       (install-template (->* () (string?) (-> (or/c '图示 '封面 '节 '致谢) any/c ... tagged-object?)))))
+                       (install-template (->* () (string?) (-> (or/c '图示 '封面 '节 '致谢 #f) any/c ... tagged-object?)))))
 
 (define current-theme-color
   (make-parameter "Firebrick"))
@@ -10,16 +10,14 @@
   (make-parameter 'roman))
 
 (define (install-template (prefix ""))
-  (define type (string->symbol (string-append prefix "学术")))
+  (define type (string->symbol (string-append prefix "幻灯片")))
 
-  (define (add-prefix symbol) (string->symbol (string-append (symbol->string type) "/" (symbol->string symbol))))
-
-  (define (create t . ls) (tag type (tag (add-prefix t) ls)))
+  (define (add-prefix symbol) (list type symbol))
+  
+  (define (create t . ls) (if t (tag (add-prefix t) ls) (tag type (car ls))))
   
   (define (handler p proc)
     (if (pict? p) p (proc p)))
-
-  (define (n:->pict subtype) (->pict subtype))
 
   (define BLACK (make-object color% "Black"))
   
@@ -28,22 +26,28 @@
   (define (smallt s (color BLACK)) (text s (cons color (current-theme-font)) (floor (/ (current-font-size) 2))))
 
   (define background (blank 1000 700))
+
+  (install type (cons/c (or/c 'lc 'cc) pict?)
+           (lambda (pair)
+             (case (car pair)
+               ((lc) (lc-superimpose background (cdr pair)))
+               ((cc) (cc-superimpose background (cdr pair))))))
   
   (define (封面->pict title member)
     (define mb (handler member normalt))
     (define tt (handler title (lambda (str) (titlet str (make-object color% (current-theme-color))))))
-    (lc-superimpose background (vl-append (current-gap-size) tt (hline (floor (/ (+ (pict-width tt) (pict-width mb)) 2)) (current-gap-size)) mb)))
+    (cons 'lc (vl-append (current-gap-size) tt (hline (floor (/ (+ (pict-width tt) (pict-width mb)) 2)) (current-gap-size)) mb)))
 
   (define (节->pict index title)
     (define id (handler index titlet))
     (define tt (handler title (lambda (str) (titlet str (make-object color% (current-theme-color))))))
-    (cc-superimpose background (vc-append (current-gap-size) id (hline (floor (/ (+ (pict-width tt) (pict-width id)) 2)) (current-gap-size)) tt)))
+    (cons 'cc (vc-append (current-gap-size) id (hline (floor (/ (+ (pict-width tt) (pict-width id)) 2)) (current-gap-size)) tt)))
 
   (define (图示->pict intr image source)
-    (cc-superimpose background (vc-append (current-gap-size)
-                                          (handler intr (lambda (str) (titlet str (make-object color% (current-theme-color)))))
-                                          image
-                                          (handler source smallt))))
+    (cons 'cc (vc-append (current-gap-size)
+                         (handler intr (lambda (str) (titlet str (make-object color% (current-theme-color)))))
+                         image
+                         (handler source smallt))))
 
   (define (致谢->pict member)
     (封面->pict "THANKS FOR YOUR LISTENING" member))
@@ -60,6 +64,5 @@
                           (list/c elem/c)))
   
   (map (lambda (n c f) (install (add-prefix n) c (make-handler f))) names contracts funcs)
-  (install type tagged-object? n:->pict)
   
   create)
